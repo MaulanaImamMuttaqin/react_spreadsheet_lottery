a3f41af7612cbdcc886bb9f1c4b4012e5b082031
=MaulanaImamMuttaqin <maulanaimammuttaqin2000@gmail.com>
2021-11-09 21:07:20 +0700

    xvvsadfsf

--- src/App.js
+++ src/App.js
@@ -1,67 +1,108 @@
 import React, { useState } from 'react'
 import * as XLSX from "xlsx";
+import CloudUpload from './components/icons/CloudUpload';
+
 
 function App() {
-  const [file, setFile] = useState("");
-  const [data, setData] = useState([]);
+  const [winner, setWinner] = useState("");
+  const [loading, setLoading] = useState(false)
+  const [states, setStates] = useState({
+    fileInfo: {},
+    data: []
+  });
 
   const filePathSet = e => {
+
     e.stopPropagation();
     e.preventDefault();
     let inputFile = e.target.files[0];
-    console.log(inputFile);
-    setFile(inputFile);
 
-    console.log(file);
+    readFile(inputFile)
+
   }
 
-  const readFile = e => {
-    let f = file;
-    let name = f.name;
+  const readFile = uploaded => {
+    let fileInfo = {
+      name: uploaded.name,
+      size: uploaded.size,
+      type: uploaded.type
+    }
+    let f = uploaded;
+    let result = []
     const reader = new FileReader();
     reader.onload = (evt) => {
+      console.log(evt)
       const bstr = evt.target.result;
       const wb = XLSX.read(bstr, { type: "binary" });
       const wsname = wb.SheetNames[0];
       const ws = wb.Sheets[wsname];
       const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
-      convertToJson(data)
+      result = convertToJson(data)
+      setStates({ fileInfo, data: result });
     };
     reader.readAsBinaryString(f);
+
   }
 
   const convertToJson = csv => {
-    var lines = csv.split("\n");
-    var result = [];
-    var headers = lines[0].split(",");
-    for (var i = 1; i < lines.length; i++) {
-      var obj = {};
-      var currentline = lines[i].split(",");
-      for (var j = 0; j < headers.length; j++) {
+    let lines = csv.split("\n");
+    let result = [];
+    let headers = lines[0].split(",");
+    for (let i = 1; i < lines.length; i++) {
+      let obj = {};
+      let currentline = lines[i].split(",");
+      for (let j = 0; j < headers.length; j++) {
         obj[headers[j]] = currentline[j];
       }
       result.push(obj);
     }
-    setData(result);
-    console.log(data)
+
+    return result
+
+  }
+
+  const pickWinner = () => {
+    let data = states.data;
+    let randomInt = Math.floor(Math.random() * data.length)
+
+    setWinner(data[randomInt]["First Name"] + " " + data[randomInt]["Last Name"])
+
   }
 
   return (
     <div className="center border border-black h-screen flex-col">
+      <label
+        htmlFor="file"
+        className="border-2 border-gray-300 px-10 py-8 rounded-2xl"
+      >
+        <CloudUpload />
+      </label>
+
+      <p>{states.fileInfo.name && `Imported: ${states.fileInfo.name}`}</p>
+
       <input
-        className=""
+        className="overflow-hidden w-0 h-0"
         type="file"
         id="file"
         onChange={(e) => filePathSet(e)}
       />
+
       <button
         className="bg-blue-300 text-white font-bold px-7 py-3 rounded-xl m-3"
-        onClick={() => readFile()}
+        onClick={() => pickWinner()}
       >
-        Read File
+        Pilih Pemenang
       </button>
+
+      {winner !== "" &&
+        <div className="text-center">
+          <p>the winner is</p>
+          <p className="text-5xl font-semibold  mt-2">{winner}</p>
+        </div>}
     </div>
   )
 }
+
+
 
 export default App
